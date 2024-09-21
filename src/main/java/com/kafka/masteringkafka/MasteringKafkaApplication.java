066b27834cd6114cfb563d1609f575c5e5aea046
Raghu <dev.raghu95@gmail.com>
2024-09-21 10:14:36 +0530

    schema generation by avro special record

--- src/main/java/com/kafka/masteringkafka/MasteringKafkaApplication.java
+++ src/main/java/com/kafka/masteringkafka/MasteringKafkaApplication.java
@@ -1,54 +1,25 @@
 package com.kafka.masteringkafka;
 
+import com.avro.model.Customer;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Instant;
+import java.time.LocalDate;
+
 public class MasteringKafkaApplication {
 
 	public static void main(String[] args) {
-
-		Schema.Parser parser = new Schema.Parser();
-		Schema schema = parser.parse("{\n" +
-				"    \"type\": \"record\",\n" +
-				"    \"namespace\": \"com.customer\",\n" +
-				"    \"name\": \"customer\",\n" +
-				"    \"fields\": [\n" +
-				"      {\n" +
-				"        \"type\": [\"null\",\"string\"],\"name\": \"name\",\"default\": null\n" +
-				"      },\n" +
-				"      {\n" +
-				"        \"type\": \"int\",\n" +
-				"        \"name\": \"age\",\n" +
-				"        \"default\": 0\n" +
-				"      },\n" +
-				"      {\n" +
-				"        \"name\": \"status\",\n" +
-				"        \"type\": {\n" +
-				"          \"name\": \"Status\",\n" +
-				"          \"type\": \"enum\",\n" +
-				"          \"symbols\": [\"ACTIVE\",\"BLOCKED\",\"SUSPENDED\",\"INACTIVE\",\"NEW\"]\n" +
-				"        }\n" +
-				"      },\n" +
-				"      {\n" +
-				"        \"name\": \"lastLoggedIn\",\n" +
-				"        \"type\": {\n" +
-				"          \"type\": \"long\",\n" +
-				"          \"logicalType\": \"timestamp-millis\"\n" +
-				"        },\n" +
-				"        \"doc\": \"User last logged in time\"\n" +
-				"      }\n" +
-				"    ]\n" +
-				"  }");
-		GenericRecordBuilder builder = new GenericRecordBuilder(schema);
-		builder.set("name","Raghu");
-		builder.set("status",Status.ACTIVE);
-		builder.set("age",30);
-		builder.set("lastLoggedIn",System.currentTimeMillis());
-		GenericRecord record = builder.build();
-		System.out.println(record);
+		//this builder created by avro plugin and this class extends SpecificRecordBase
+		Customer.Builder customerBuilder = Customer.newBuilder();
+		customerBuilder.setAge(30);
+		customerBuilder.setName("James");
+		customerBuilder.setStatus(com.avro.model.Status.NEW);
+		customerBuilder.setLastLoggedIn(Instant.ofEpochSecond(System.currentTimeMillis()));
+		System.out.println(customerBuilder.getAge());
 	}
 
 
